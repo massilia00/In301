c8e80c831d71f12964978c512cc041693792271b
massilia00 <moungadmassilia.vfc@gmail.com>
2019-11-02 16:40:54 +0100

    correction degug.c

--- td0/debug-1.c
+++ td0/debug-1.c
@@ -5,7 +5,7 @@
 
 //fonction factorielle
 int factorielle(int x){
-  int res;
+  int res = x;
   if(x == 0) return 0;
   while(x > 1){
     res *= (x-1);
@@ -19,7 +19,7 @@ int somme(int x){
   int res = 0;
   int i = 0;
   while(i <= x){
-    i++;       /*incrementation 
+    i++;       /*incrementation */ 
     res += i-1;/* fin de boucle */
   }
   return res;
@@ -27,7 +27,7 @@ int somme(int x){
 
 //fonction maximum
 int maximum(int x, int y){
-  if(x = y || y > x)
+  if(y > x)
     return y;
   else 
     return x;
